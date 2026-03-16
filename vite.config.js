299f7dc2ecad1c7701b140a6b1bb87445756b958
Aditya Ojha <ojhaa7334@gmail.com>
2026-03-16 18:39:02 +0530

    chore(seo): add robots.txt, sitemap and meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({ hostname: 'https://eagle-brothers-fitness-6fc9.vercel.app' })
+  ],
   server: {
     proxy: {
       '/api': {
